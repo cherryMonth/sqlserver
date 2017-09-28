c952c321b165317dc4cf41f1e16f85ce5fadb0a4
CherryMonth <1115064450@qq.com>
2017-09-28 15:42:15 +0800

    添加了初始化信息

--- data_query.sql
+++ data_query.sql
@@ -6,4 +6,19 @@
 -- select Sname,Sno,Sdept from Student;
 
 -- 查询全体学生的详细记录
-select * from Student;
+-- select * from Student;
+
+-- 查询全体学生的姓名，学号，所在系
+-- select Sname,Sno,Sdept from Student;
+
+-- 查询全体学生的详细记录
+-- select * from Student;
+
+-- 查询全体学生的姓名及其出生年份
+-- select Sname,2014-Sage from Student;
+
+-- 查询全体学生的姓名，出生年份和所在院系，要求用小写字母表示系名。
+-- select Sname, 'Year of Birth', 2014-Sage, LOWER(Sdept) from Student;
+
+-- 
+
---
+++ init_database.sql
@@ -0,0 +1,37 @@
+/* 
+
+初始化Student表
+insert into Student values ('201215121', '李勇', '男',20 , 'CS', '');
+insert into Student values ('201215122', '刘晨', '女',19, 'CS', '');
+insert into Student values ('201215123', '王敏', '女',18 , 'MA', '');
+insert into Student values ('201215125', '张立', '男',19 , 'IS', '');
+
+*/
+
+
+
+/*
+
+初始化 Course表
+insert into Course values('2','数学',null,2);
+insert into Course values('6','数据处理',null,2);
+insert into Course values('7','PASCAL语言','6',4);
+insert into Course values('5','数据结构','7',4);
+insert into Course values('1','数据库','5',4);
+insert into Course values('3','信息系统','1',4);
+insert into Course values('4','操作系统','6',3);
+*/
+
+
+
+/*
+
+初始化SC 表
+insert into SC values('201215121','1',92);
+insert into SC values('201215121','2',85);
+insert into SC values('201215121','3',88);
+insert into SC values('201215122','2',90);
+insert into SC values('201215122','3',80);
+
+*/
+
