3cb556ac2a314bffcd31dedcc64b6eef5252d44b
CherryMonth <1115064450@qq.com>
2017-10-10 21:54:18 +0800

    as

---
+++ first.sql
@@ -0,0 +1,65 @@
+﻿-- drop table Student;
+-- drop table Course;
+-- drop table SC;
+
+create table Student 
+(Sno char(20) primary key,
+Sname char(20) unique,
+Ssex char(2),
+Sage smallint,
+Sdept char(20)
+);
+
+create table Course /* 课程表 */
+(Cno char(4) primary key,
+Cname char(40) not null, /* 列级完整性约束条件 Cname不能取空值 */
+Cpno char(4), 
+/* Cpno 的含义是先修课 */
+Credit smallint,
+Foreign key(Cpno) references Course(Cno) /* Cpno 是外码,被参照表是Course, 被参照列是Cno */
+);
+
+
+create table SC /* 学生选课表 */
+(
+  Sno char(20),
+  Cno char(4),
+  Grade smallint,
+  primary key(Sno,Cno), /* 主键由两个属性构成 */
+  foreign key(Sno) references Student(Sno),
+  foreign key(Cno) references Course(Cno)
+);
+
+insert into Student values ('201215121', '李勇', '男',20 , 'CS');
+insert into Student values ('201215122', '刘晨', '女',19, 'CS');
+insert into Student values ('201215123', '王敏', '女',18 , 'MA');
+insert into Student values ('201215125', '张立', '男',19 , 'IS');
+insert into Student values ('201516920711', '宋建', '男',20 , 'SE');
+
+insert into Course values('2','数学',null,2);
+insert into Course values('6','数据处理',null,2);
+insert into Course values('7','PASCAL语言','6',4);
+insert into Course values('5','数据结构','7',4);
+insert into Course values('1','数据库','5',4);
+insert into Course values('3','信息系统','1',4);
+insert into Course values('4','操作系统','6',3);
+
+insert into SC values('201215121','1',92);
+insert into SC values('201215121','2',85);
+insert into SC values('201215121','3',88);
+insert into SC values('201215122','2',90);
+insert into SC values('201215122','3',80);
+
+-- create CLUSTERED index Stuno on Student(Sname); /* 创建非唯一聚簇索引 */
+
+-- create unique CLUSTERED index Stuno on Student(Sname); /* 创建唯一聚簇索引 */
+
+-- create index Stuno on Student(Sname); /* 创建非唯一非聚簇索引 */
+
+-- create unique index Stuno on Student(Sname); /* 创建唯一非聚簇索引 */
+
+
+
+
+
+
---
+++ second.sql
@@ -0,0 +1,36 @@
+﻿-- （1）	在产品表(Products)中找出库存大于30的产品的所有信息：
+-- select * from Products where UnitsInStock > 30;
+
+-- （2）	查询顾客表(Customers)中所有不重复的所在城市
+-- select City from Customers group by City having count(*)=1;
+
+--（3）	在订单表(Orders)中找出运费在10到50（含10和50）之间的订单编号、顾客编号和职员编号
+-- select OrderID,CustomerID, EmployeeID from Orders where Freight >=10 and Freight <=50;
+
+--（4）	在顾客表(Customers)中找出所在城市为London的联系人名和公司名
+-- select ContactName,CompanyName from Customers where City='London';
+
+--（5）	在顾客表(Customers)中找出所在城市为London、Madrid、Torino和Paris的顾客编号及电话
+-- select CustomerID,Phone from Customers where City='London' or City='Madrid' or City='Torino' or City='Paris';
+
+--（6）	在订单表(Orders)中找出国籍不是Brazil、Spain和Mexico的订单编号和订货日期
+-- select OrderID,OrderDate from Orders where ShipCountry!='Brazil' and ShipCountry!='Spain' and ShipCountry!='Mexico';
+
+--（7）	在产品表(Products)中找出单位数量中有box的产品名和产品编号
+-- select ProductID,ProductName from Products where QuantityPerUnit like '%box%';
+
+--（8）	在顾客表(Customers)中找出公司名的首字母为F的顾客编号和联系人名
+-- select CustomerID,ContactName from Customers where CompanyName like 'F%';
+
+--（9）	在顾客表(Customers)中找出公司名的首字母为F，第5位为k的顾客编号和联系人名
+-- select CustomerID,ContactName from Customers where CompanyName like 'F___k%';
+
+--（10）统计在'1997-10-1' 到'1997-10-7'期间，订单中每个员工的订单数，并按订单数降序排序。
+-- select EmployeeID,OrderDate from Orders where OrderDate >= '1997-10-1' and OrderDate <= '1997-10-7';
+-- select EmployeeID ,COUNT(*) from Orders where  OrderDate >= '1997-10-1' and OrderDate <= '1997-10-7' group by EmployeeID having COUNT(*) >=0 order by COUNT(*) desc ;
+
+-- （11）请查询平均价格在及以上的产品类型
+-- select AVG(UnitPrice) from (select CategoryID from Products group by CategoryID);
+
+--select CategoryID ,UnitPrice from Products;
+--select CategoryID ,UnitPrice from Products group by CategoryID;
